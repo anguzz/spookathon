8efd2f70caf133766dc04260bf638a2ccf4bbba3
Angel Santoyo <santoyox714@gmail.com>
2023-10-27 15:42:21 -0700

    add basic background and spawn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Cowboy from './cowboy.svelte';   
+import { onMount } from 'svelte';
+  
+let spawnPoints = [
+    { left: 525, top: 200 },//top left
+    { left: 900, top: 200 },//top right
+    { left: 370, top: 420 },//bottom left
+    { left: 1000, top: 420 },//bottom right
+    { left: 690, top: 420 }//middle
+  ];
+
+  let cowboys = [];
+  let score = 0;
+
+  function spawnCowboy() {
+    // Choose a random spawn point from the predefined spawnPoints array
+    const randomSpawnPoint = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
+    cowboys = [...cowboys, { left: randomSpawnPoint.left, top: randomSpawnPoint.top }];
+  }
+
+ 
+  
+   
+  function handleClick() {
+    score++;
+    alert(`You clicked a cowboy! Your score: ${score}`);
+  }
+  
+    onMount(() => {
+      setInterval(spawnCowboy, 2000);
+    });
+  </script>
+  
+ 
+  <div id="bg">
+
+
+        <div class="score">Score: {score}</div>
+        {#each cowboys as { left, top }, index}
+          <Cowboy {left} {top} />
+        {/each}
+</div>
+
+   <style>
+    
+
+    .score {
+    position: fixed;
+    top: 10px;
+    left: 10px;
+    color:red;
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+
+	#bg {
+	  width: 100%;
+	height: 100vh;
+	color: #fff;
+	background-image: url("/src/background.jpg");	
+    background-size: 100% 100%;
+	position: relative;
+    overflow-y: hidden;
+	overflow: hidden;
+	}
+	* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+    scrollbar-width: thin; /* For Firefox */
+    scrollbar-color: transparent transparent; /* For Firefox */
+    -ms-overflow-style: none; /* For Internet Explorer and Edge */
+}
+
+  </style>
---
+++ src/routes/cowboy.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let left;
+  export let top;
+
+  function handleClick() {
+    alert('You clicked a cowboy!');
+  }
+</script>
+
+<div class="cowboy" style="left: {left}px; top: {top}px" on:click={handleClick}></div>
+
+<style>
+  .cowboy {
+    width: 100px;
+    height: 100px;
+    position: absolute;
+    background-image: url("/src/cowboy.jpg");
+    background-size: cover;
+    cursor: pointer;
+  }
+</style>
