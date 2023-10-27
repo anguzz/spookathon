384ce6d56171ffcf5bd6839c82b3fd9f9c5f837f
Angel Santoyo <santoyox714@gmail.com>
2023-10-27 16:30:37 -0700

    add timer
    -breaks after every run but will keep running on client

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,9 +14,11 @@
     let cowboys = [];
     let frogs = [];
     let score = 0;
+
+  
   
     function isTooClose(newSpawnPoint, existingEntities) {
-      const minDistance = 150; // Minimum distance between entities
+      const minDistance = 5; // Minimum distance between entities
       for (const entity of existingEntities) {
         const distance = Math.sqrt(Math.pow(newSpawnPoint.left - entity.left, 2) + Math.pow(newSpawnPoint.top - entity.top, 2));
         if (distance < minDistance) {
@@ -26,28 +28,39 @@
       return false; // The new spawn point is not too close to any existing entity
     }
   
+ 
     function spawnCowboy() {
-      const randomSpawnPoint = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
-      if (!isTooClose(randomSpawnPoint, [...frogs, ...cowboys])) {
-        cowboys = [...cowboys, { id: Date.now(), left: randomSpawnPoint.left, top: randomSpawnPoint.top }];
-      }
+    const randomSpawnPoint = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
+    if (!isTooClose(randomSpawnPoint, [...frogs, ...cowboys])) {
+      const cowboy = { id: Date.now(), left: randomSpawnPoint.left, top: randomSpawnPoint.top };
+      cowboys = [...cowboys, cowboy];
+
+      // Set a timer to remove the cowboy after 2 seconds
+      setTimeout(() => {
+        cowboys = cowboys.filter(cowboy => cowboy.id !== cowboy.id);
+      }, 2000);
     }
-  
-    function spawnFrog() {
-      const frogNames = ['Hoppy', 'Ribbit', 'Croaky', 'Jumpy', 'Lily']; // Frog names
-      const randomSpawnPoint = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
-      const newFrog = {
-        id: Date.now(),
-        name: frogNames[Math.floor(Math.random() * frogNames.length)], // Random frog name
-        left: randomSpawnPoint.left,
-        top: randomSpawnPoint.top
-      };
-  
-      if (!isTooClose(randomSpawnPoint, [...frogs, ...cowboys])) {
-        frogs = [...frogs, newFrog];
-      }
+  }
+
+  function spawnFrog() {
+    const frogNames = ['Hoppy', 'Ribbit', 'Croaky', 'Jumpy', 'Lily']; // Frog names
+    const randomSpawnPoint = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
+    const newFrog = {
+      id: Date.now(),
+      name: frogNames[Math.floor(Math.random() * frogNames.length)], // Random frog name
+      left: randomSpawnPoint.left,
+      top: randomSpawnPoint.top
+    };
+
+    if (!isTooClose(randomSpawnPoint, [...frogs, ...cowboys])) {
+      frogs = [...frogs, newFrog];
+
+      // Set a timer to remove the frog after 2 seconds
+      setTimeout(() => {
+        frogs = frogs.filter(frog => frog.id !== newFrog.id);
+      }, 2000);
     }
-  
+  }
     function handleCowboyClick(clickedId) {
       cowboys = cowboys.filter(cowboy => cowboy.id !== clickedId);
       score++;
@@ -63,10 +76,36 @@
       setInterval(spawnCowboy, 2000);
       setInterval(spawnFrog, 3000);
     });
+
+
+    let timer = 30; // Set the initial timer value in seconds
+
+    const cowboyInterval = setInterval(spawnCowboy, 2000);
+  const frogInterval = setInterval(spawnFrog, 3000);
+
+function updateTimer() {
+  if (timer > 0) {
+    timer--; // Decrement the timer every second
+  } else {
+    clearInterval(timerInterval); // Stop the timer when it reaches zero
+    clearInterval(cowboyInterval); // Stop spawning cowboys
+    clearInterval(frogInterval); // Stop spawning frogs
+    alert("Game Over! Your final score: " + score);
+    timer=0;
+
+  }
+}
+
+
+
+
+// Start the timer when the component is mounted
+const timerInterval = setInterval(updateTimer, 1000);
   </script>
   
   <div id="bg">
     <div class="score">Score: {score}</div>
+    <div class="timer">Time Left: {timer} seconds</div>
     {#each cowboys as { id, left, top }, index}
       <Cowboy {id} {left} {top} onCowboyClick={handleCowboyClick} />
     {/each}
@@ -106,5 +145,14 @@
       position: relative;
       overflow-y: hidden;
     }
+
+    .timer {
+    position: fixed;
+    top: 40px;
+    left: 10px;
+    color: blue;
+    font-size: 24px;
+    font-weight: bold;
+  }
   </style>
   
